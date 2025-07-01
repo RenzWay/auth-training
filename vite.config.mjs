b47389a9ee38d42fafceb2755e8e45a3988d434a
RenzWay <dikakamza@gmail.com>
2025-07-01 18:54:28 +0700

    beta: again firebase

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,6 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [viteReact(), tailwindcss()],
   build: {
-    outDir: "build",
+    outDir: "docs",
   },
 });
